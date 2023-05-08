e3649f2d04142c7ba1c1da3c11ff006441147fa8
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-05-08 21:20:54 +0300

    Template

--- routes/blogs.js
+++ routes/blogs.js
@@ -5,5 +5,8 @@ router.get('/', function (req, res, next) {
 
 });
 
+router.get('/new', function (req, res, next) {
+    res.render('newBlog', { title: 'Login - Brain Data' });
+});
 
 module.exports = router;
